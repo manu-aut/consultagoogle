7fead3fc5779aa7bb0ab89fd242035e249d82f3f
juancho <psstv@gmail.com>
2019-08-28 15:49:17 -0500

    ultimo commit bebe

---
+++ src/main/java/co/com/psstv/TN_Pruebaz_/interactions/Esperar.java
@@ -0,0 +1,24 @@
+package co.com.psstv.TN_Pruebaz_.interactions;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Interaction;
+
+public class Esperar implements Interaction {
+
+	@Override
+	public <T extends Actor> void performAs(T actor) {
+
+		/*
+		 * Amigo "jefe" experto o algo asi no te desgastes mandando hacer metodos sobre
+		 * los metodos tratando de validar un #target visible con ciclos que recorran
+		 * interacciones de tiempos vistos por el..actor para evitar el uso de
+		 * three.sleep or waitUntilVisible ... peor aun no encapsules three.sl en
+		 * metodos
+		 * 
+		 * pasate a protractor si si tienes un nivel regular en java ahora js? te has
+		 * preguntado Synchronous or Asynchronous? i m sorry nodejs typesCript is now
+		 */
+
+	}
+
+}
--- src/main/java/co/com/psstv/TN_Pruebaz_/utils/Esperar.java
+++
@@ -1,5 +0,0 @@
-package co.com.psstv.TN_Pruebaz_.utils;
-
-public class Esperar {
-
-}
